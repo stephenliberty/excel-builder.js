a63bfbecdea8bc8ec89de622f24bbae0deb4d50c
Stephen <stephen@liberty-irm.com>
2012-09-20 13:34:44 -0500

    Forgot file

---
+++ Excel/SharedStrings.js
@@ -0,0 +1,42 @@
+"use strict";
+/**
+ * @module Excel/SharedStrings
+ */
+define(['underscore', './util'], function (_, util) {
+    var sharedStrings = function () {
+        this.strings = {};
+        this.stringArray = [];
+        this.id = _.uniqueId('SharedStrings');
+    }
+    _.extend(sharedStrings.prototype, {
+        addString: function (string) {
+            this.strings[string] = this.stringArray.length;
+            this.stringArray[this.stringArray.length] = string;
+            return this.strings[string];
+        },
+        
+        toXML: function () {
+            var doc = util.createXmlDoc(util.schemas.spreadsheetml, 'sst');
+            var sharedStringTable = doc.documentElement;
+            this.stringArray.reverse();
+            var l = this.stringArray.length;
+            sharedStringTable.setAttribute('count', l);
+            sharedStringTable.setAttribute('uniqueCount', l);
+            
+            var template = doc.createElement('si');
+            var templateValue = doc.createElement('t');
+            templateValue.appendChild(doc.createTextNode('--placeholder--'));
+            template.appendChild(templateValue);
+            var strings = this.stringArray;
+            
+            while (l--) {
+                var clone = template.cloneNode(true);
+                clone.firstChild.firstChild.nodeValue = strings[l];
+                sharedStringTable.appendChild(clone);
+            }
+            
+            return doc;
+        }
+    });
+    return sharedStrings;
+});
