b42d6c025e58ca44590753ef6c4b316dd9b74676
Thomas Meyer <meyertee@gmail.com>
2014-02-11 16:53:34 +0100

    add requirejs grunt task to minify to a single file

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		requirejs: {
+			compile: {
+				options: {
+					baseUrl: ".",
+					name: "<%= pkg.name %>",
+					/*optimize: "none",*/
+					paths: {
+						"requireLib": "node_modules/requirejs/require",
+						"underscore": "node_modules/underscore/underscore-min",
+						"JSZip": "node_modules/jszip/jszip"
+					},
+					shim: {
+						"underscore": {
+							"exports": '_'
+						},
+						"JSZip": {
+							"exports": "JSZip"
+						}
+					},
+					include: "requireLib",
+					out: "build/<%= pkg.name %>.min.js"
+				}
+			}
+		}
+	});
+
+	// Load the plugin that provides the "requirejs" task.
+	grunt.loadNpmTasks('grunt-contrib-requirejs');
+
+	// Default task(s).
+	grunt.registerTask('default', ['requirejs']);
+};
